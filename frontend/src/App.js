b891b99f534865e5405f209ee57a6e2c2289fbc9
Cyb3rWr4i7h <jayesh.choudhary.skr@gmail.com>
2025-01-26 18:56:32 +0530

    changed text reader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,21 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialIcons, MaterialCommunityIcons, FontAwesome } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
-import HomeScreen from './screens/HomeScreen'; // Ensure this path is correct
-import ObjectDetectionScreen from './screens/ObjectDetectionScreen'; // Ensure this path is correct
-import TextReaderScreen from './screens/TextReaderScreen'; // Ensure this path is correct
-import VoiceAssistantScreen from './screens/VoiceAssistantScreen'; // Ensure this path is correct
-import NavigationAssistantScreen from './screens/NavigationAssistantScreen'; // Ensure this path is correct
+import HomeScreen from './screens/HomeScreen';
+import ObjectDetectionScreen from './screens/ObjectDetectionScreen';
+import TextReaderScreen from './screens/TextReaderScreen';
+import VoiceAssistantScreen from './screens/VoiceAssistantScreen';
+import NavigationAssistantScreen from './screens/NavigationAssistantScreen';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      {/* <StatusBar style="auto" /> */}
       <Tab.Navigator
         initialRouteName="Home"
         screenOptions={({ route }) => {
-          console.log(route.name); // Check if screen names are correctly passed
+          console.log(route.name);
           return {
             tabBarStyle: { backgroundColor: '#f8f9fa' },
             tabBarActiveTintColor: '#007bff',
